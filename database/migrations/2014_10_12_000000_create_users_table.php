d69c9006d9a0e60680a8ac2d172d13ce9f2848f0
Rizwan Bashir <84863970+Rizwan-Basheer@users.noreply.github.com>
2023-02-09 12:32:04 +0500

    Add platform column from user_orders and user_type column from user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('username');
             $table->string('email')->unique();
+            $table->string('user_type');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
