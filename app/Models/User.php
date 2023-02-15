7250ab574d599857573b03ea7331f938b72f2738
zaheer48 <emailtobabar1@gmail.com>
2023-02-15 12:23:42 +0500

    dp dynamic

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable
         'status', 
         'password_hint',
         'create_by',
+        'profile_photo'
     ];
 
     /**
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -90,10 +90,10 @@
             @if (Auth::check())
             <li class="dropdown notification-list topbar-dropdown">
                 <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    @if (Auth::user()->profile_url)
-                    <img src="{{ asset('storage/images/'.Auth::user()->profile_url)}}" alt="user-image" class="rounded-circle">
-                  @else
-                    <img src="{{URL::asset('assets/images/users/user-1.jpg')}}" alt="user-image" class="rounded-circle">
+                    @if (Auth::user()->profile_photo)
+                        <img src="{{ asset('storage/images/'.Auth::user()->profile_photo)}}" alt="user-image" class="rounded-circle">
+                    @else
+                        <img src="{{URL::asset('assets/images/users/avatar.png')}}" alt="user-image" class="rounded-circle">
                     @endif
                     @if(Auth::check())
                     <span class="pro-user-name ms-1">
